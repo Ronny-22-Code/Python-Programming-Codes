4a2e4b1e09fe2c380b0a203dc59c99052773f071
Rohan Sethi <mailtorohansethi@gmail.com>
2019-01-13 19:15:27 +0530

    Python File

---
+++ Func_Mod.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#This code serves as the module code to Miscellaneous Programming in Python.
+
+
+# In[ ]:
+
+
+def c_area(r):
+    
+    area = 3.14*r*r
+    
+    print(area)
+    
+def s_area(s):
+    
+    area=s*s
+    
+    print(area)
+
+def r_area(l,b):
+    
+    area=l*b
+    
+    print(area)
+
+def cyl_area(r,l):
+    
+    area=2*3.14*r*(r+l)
+    
+    print(area)
+    
+def cubo_area(l,b,h):
+    
+    area=2*(l*b+b*h+h*l)
+    
+    print(area)
+    
+def sph_area(r):
+    
+    area=4*3.14*r**2
+    
+    print(area)
+    
+def cube_area(s):
+    
+    area=6*s**2
+    
+    print(area)
+    
+    
+
